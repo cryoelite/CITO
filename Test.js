1dd9fa77af3797f5ae47709478ee82d91352b3f8
Arvind Sagar <arvindsagar702@gmail.com>
2020-12-07 07:10:01 +0530

    koa

--- Test.js
+++ Test.js
@@ -1,14 +1,7 @@
+const koa= require('koa');
+const app=new koa();
 
-var mdt = require('./Module1.js');
-var http = require('http');
-var url = require('url');
-var page = require('fs');
-http.createServer(function (req, res) {
-    page.readFile('Web.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        var q = url.parse(req.url, true).query;
-        res.write(q.year + " " + q.month);
-        res.end(data);
-    });
-
-}).listen(8080);
+app.use(async ctx =>{
+    ctx.body='Juice world';
+});
+app.listen(8080);
