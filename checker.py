0c09cab56b95382ab2ab38a468f86cf84753a394
Arvind Sagar <arvindsagar702@gmail.com>
2020-12-10 23:31:40 +0530

    Create checker.py

---
+++ checker.py
@@ -0,0 +1,11 @@
+
+import hashlib
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+cp2077="REDprelauncher.exe"
+print(md5(cp2077))
