523cf626c6f90b6dd6ff8a28ae2a957afd9435d7
Arvind Sagar <arvindsagar702@gmail.com>
2020-12-22 23:40:14 +0530

    bitonic merge sort

--- VisualStudioAlpha/main.cpp
+++ VisualStudioAlpha/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+#include<chrono>
 
-class P
+class Timer
 {
-public:
-	static inline int c{ 20 };
-	void f1(double a);
-	static void f2(double b)
+	std::chrono::steady_clock::time_point startPoint;
+	Timer()
 	{
-
+		startPoint = std::chrono::steady_clock::now();
+	}
+	void calcTime()
+	{
+		auto endPoint = std::chrono::steady_clock::now();
+		std::cout << "\n Time taken: " << std::chrono::duration_cast<std::chrono::milliseconds>(endPoint - startPoint).count() << std::endl;
 	}
 };
+
+void sortedFuck(int* arr)
+{
+	using namespace std;
+	cout << arr[5];
+
+}
+
 
 int main()
 {
 	using namespace std;
-	
-	P::c = 2;
-
+	int arr[]{ 3,7,4,8,6,2,1,5 };
+	sortedFuck(arr);
 	return 0;
 }
