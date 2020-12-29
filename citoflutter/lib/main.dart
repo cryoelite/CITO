c7cb5ea476559ac5007d6b25a0e0871b4236a4a6
Arvind Sagar <arvindsagar702@gmail.com>
2020-12-29 23:39:20 +0530

    Update main.dart

--- citoflutter/lib/main.dart
+++ citoflutter/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:citoflutter/route_home.dart';
-
 void main() => runApp(StartApp());
-
 class StartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
