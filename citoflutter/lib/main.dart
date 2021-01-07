e2d7aa13ab2046f75ed4c6860c09959125c15ac9
Arvind Sagar <arvindsagar702@gmail.com>
2021-01-07 22:11:37 +0530

    still learning perspective

--- citoflutter/lib/main.dart
+++ citoflutter/lib/main.dart
@@ -7,15 +7,23 @@ main() {
 }
 
 class Page1 extends StatelessWidget {
+  Offset offset1 = Offset(0.4, 0.7);
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go!'),
-          onPressed: () {
-            Navigator.of(context).push(_createRoute());
-          },
+    return Transform(
+      transform: Matrix4.identity()
+        ..setEntry(3, 2, 0.001)
+        ..rotateX(offset1.dx)
+        ..rotateY(offset1.dy),
+      alignment: FractionalOffset.center,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: ElevatedButton(
+            child: Text('Go!'),
+            onPressed: () {
+              Navigator.of(context).push(_createRoute());
+            },
+          ),
         ),
       ),
     );
