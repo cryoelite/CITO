cab5a7d8d1b7b21d572622b435f24e8807ce0a4a
Arvind Sagar <arvindsagar702@gmail.com>
2021-01-30 23:30:59 +0530

    learning stuff

---
+++ VisualStudioBeta/a1.cpp
@@ -0,0 +1,13 @@
+#include "a1.h"
+
+Pop::Pop()
+{
+	a = 2;
+	b = 3;
+}
+
+Pop::~Pop()
+{
+	a = 0;
+	b = 0;
+}
---
+++ VisualStudioBeta/a1.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Pop
+{
+	public:
+	int a;
+	int b;
+	Pop();
+
+	virtual ~Pop();
+};
