a5eceaf0152d7666a37a47257cc41ca33300306c
Arvind Sagar <arvindsagar702@gmail.com>
2021-02-16 23:26:37 +0530

    Testing StreamController

--- test_dart/bin/test_dart.dart
+++ test_dart/bin/test_dart.dart
@@ -1,12 +1,53 @@
 import 'dart:async';
 
+import 'dart:isolate';
+
 class Lox {
-  int val;
+  int val = 0;
   StreamController<int> streamController;
   Timer timer;
-  Lox(this.streamController);
+  Lox() {
+    streamController = StreamController<int>(
+        onListen: _startStream,
+        onCancel: _stopStream,
+        onPause: _stopStream,
+        onResume: _startStream);
+  }
+
+  Stream<int> call() {
+    return streamController.stream;
+  }
+
+  void _startStream() {
+    timer = Timer.periodic(
+      Duration(seconds: 5),
+      (bkTime) {
+        val++;
+        streamController.add(val);
+      },
+    );
+  }
+
+  void _stopStream() {
+    timer.cancel();
+    streamController.close();
+    print('Stopped');
+  }
+}
+
+Future<void> doStuff() async {
+  final lox = Lox();
+  final stream = lox();
+  final subscriber = stream.listen(
+    (event) {
+      print(event);
+    },
+  );
+  await Future<void>.delayed(Duration(seconds: 30));
+  await subscriber.cancel();
 }
 
 void main() {
-  print('yo');
+  Future<void>(doStuff).then((_) => print('done'));
+
 }
