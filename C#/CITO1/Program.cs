dd840aed4ef36550074a782da606943f35d7de82
Arvind Sagar <arvindsagar702@gmail.com>
2021-03-24 12:28:17 +0530

    citoTest

--- C#/CITO1/Program.cs
+++ C#/CITO1/Program.cs
@@ -1,33 +1,41 @@
 using System;
+using System.Data.Entity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-// Compiler version 4.0, .NET Framework 4.5
 
-
-namespace Dcoder
+public class SContext : DbContext
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            bool shouldResume = true;
-            while (shouldResume)
-            {
-                Console.WriteLine("Enter Company Name and Registration Date information seperated by comma (,):");
-                string inp = Console.ReadLine();
-                string[] inpList = inp.Split(',');
-                string name = inpList[0];
-                string date = inpList[1];
+    public SContext() { }
 
-                string[] dateListString = date.Split('-');
-                int[] dateList=new int[3];
-                foreach (var x in dateListString)
-                    dateList.Append(Convert.ToInt32(x));
-                DateTime dateTime = Convert.ToDateTime(date);
-                Console.WriteLine(dateTime);
-            }
-        }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<StudentAddress> StudentAddresss { get; set; }
+
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int RollNo { get; set; }
+}
+
+public class StudentAddress
+{
+    public List<int> HouseNos { get; set; }
+}
+
+
+public class Heya
+
+{
+
+
+
+    public static void Main()
+    {
+        SContext context = new SContext();
+        context.Students.Add(new Student() { Name = "Ak", RollNo = 12 });
+        context.SaveChanges();
+
+
+        Console.WriteLine("Lol");
     }
 }
--- C#/CITO2/Program.cs
+++ C#/CITO2/Program.cs
@@ -1,51 +1,41 @@
 ﻿using System;
-using System.IO;
-using System.Net;
+using System.Data.Entity;
 using System.Collections.Generic;
-using System.Linq;
 
-internal class SavingsAccount
+public class SContext : DbContext
 {
-   public int num;
-    public string pp;
-    public int ll;
-    public SavingsAccount(int one, string two, int three) => (num,pp,ll) = (one,two,three);
+    public SContext() { }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<StudentAddress> StudentAddresss { get; set; }
 
 }
-class Program
+
+public class Student
 {
+    public string Name { get; set; }
+    public int RollNo { get; set; }
+}
+
+public class StudentAddress
+{
+    public List<int> HouseNos { get; set; }
+}
+
+
+public class Heya
+
+{
+
+
 
     public static void Main()
     {
-
-        /*WebClient client = new WebClient();
-
-        // Add a user agent header in case the
-        // requested URI contains a query.
-
-        client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-
-        Stream data = client.OpenRead("https://na.finalfantasyxiv.com/lodestone/worldstatus/");
-        StreamReader reader = new StreamReader(data);
-        string s = reader.ReadToEnd();
-        Console.WriteLine(s);
-        data.Close();
-        reader.Close();*/
-        GetAccountDetails("details.txt");
-    }
+        SContext context = new SContext();
+        context.Students.Add(new Student() { Name = "Ak", RollNo = 12 });
+        context.SaveChanges();
 
 
-    public static void GetAccountDetails(String filename)
-    {
-        string[] lines = System.IO.File.ReadAllLines(filename);
-        List<SavingsAccount> savingsList = new List<SavingsAccount>();
-        foreach (var line in lines)
-        {
-            string[] text = line.Split(",");
-            savingsList.Add(new SavingsAccount(Convert.ToInt32(text[0].Trim()), text[1].Trim(), Convert.ToInt32(text[2].Trim())));
-        }
-        Console.WriteLine("All Account Details Added To List Successfully");
-        Console.WriteLine(savingsList[2].pp);
-        filecontent= System.IO.File.ReadAllText(filename);
+        Console.WriteLine("Lol");
     }
 }
