b6b28b64691d6cac48ccd3a1ebf2beea6022efbd
Arvind Sagar <arvindsagar702@gmail.com>
2021-04-06 23:53:00 +0530

    testing new stuff

--- JS/Test.js
+++ JS/Test.js
@@ -1,3 +1,11 @@
-var qpt = "QualityPointTechnologies";
-var pattern = new RegExp("TECHNOLOGIES","i");
-console.log(eval((20*4)));
+const fetch = require('node-fetch');
+
+(async () => {
+    const url='https://api.gbif.orgv1/species/match/';
+    const addedParams='&verbose=true&kingdom=Plantae&name=Agathis%20montana';
+    const uri=url+addedParams;
+    const response = await fetch(uri);
+    const json = await response.json();
+    console.log(json);
+    console.log(uri);
+})();
