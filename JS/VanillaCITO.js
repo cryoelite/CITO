92b33cf9a1480d4780babbf0afc26f0dca1a651e
Arvind Sagar <arvindsagar702@gmail.com>
2021-05-11 23:26:42 +0530

    Learning JS

---
+++ JS/VanillaCITO.js
@@ -0,0 +1,9 @@
+'use strict';
+let pp=new function () {
+    this.name = "name";
+    this.isAdmin = false;
+  };
+
+
+console.log(`a is ${pp.name}`);
+
---
+++ Kotlin/src/Def.kt
@@ -0,0 +1,6 @@
+
+fun sooom(a: Int,b: Int) = a+b;
+fun main()
+{
+    println("Hello World"+ sooom(2, 3));
+}
