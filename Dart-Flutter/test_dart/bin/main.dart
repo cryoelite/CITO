6925e4944cb8793ac60093bb676413e726a5b6ad
Arvind Sagar <arvindsagar702@gmail.com>
2021-08-29 16:37:21 +0530

    dart directory testing

--- Dart-Flutter/test_dart/bin/main.dart
+++ Dart-Flutter/test_dart/bin/main.dart
@@ -1,56 +1,32 @@
-import 'package:xml/xml.dart';
 import 'dart:io';
 
-Future<int> main() async {
-  final longt = 79.6079953;
-  final lat = 27.6592237;
-  print(await findDistrict(lat, longt));
-  return 0;
+int lineCount = 0;
+
+Future<void> main() async {
+  final folderPath = r'C:\Users\millify\Documents\GitHub\fawnora';
+  final dir = Directory(folderPath);
+  final dirContents = await dir.list();
+  await doStuff(dirContents);
+  print('Total Lines $lineCount');
 }
 
-Future<String> findDistrict(double posLat, double posLong) async {
-  var districtName = 'Unknown';
-  final dir = Directory('./asset');
-  if (await dir.exists()) {
-    await for (var file in dir.list()) {
-      if (file.path.endsWith('.kml')) {
-        final kmlFile = await File(file.path).readAsString();
-        final kmlParsed = XmlDocument.parse(kmlFile);
-        final name = kmlParsed.findAllElements('name').elementAt(1).innerText;
-        final coordinates =
-            kmlParsed.findAllElements('coordinates').first.innerText.split(' ');
-        final lat = <double>[];
-        final longt = <double>[];
-        coordinates.forEach((element) {
-          final elemParts = element.split(',');
-          if (elemParts.length == 3) {
-            lat.add(double.tryParse(elemParts[1]) ?? 0);
-            longt.add(double.tryParse(elemParts[0]) ?? 0);
-          }
-        });
-        if (pnpoly(lat.length, lat, longt, posLat, posLong)) {
-          districtName = name;
-          break;
-        }
-        /* output +=
-        '${name}\nminLat: ${lat.reduce(min)}-----minLong: ${longt.reduce(min)}\nmaxLat: ${lat.reduce(max)}-----maxLong: ${longt.reduce(max)}\n\n'; */
+Future<void> doStuff(Stream<FileSystemEntity> stream) async {
+  await for (var item in stream) {
+    final itemStat = await item.stat();
+    final itemType = itemStat.type;
+    if (itemType == FileSystemEntityType.directory) {
+      final uri = item.uri;
+      final dir = Directory.fromUri(uri);
+      final dirContents = await dir.list();
+      await doStuff(dirContents);
+    } else if (itemType == FileSystemEntityType.file) {
+      if (item.path.endsWith('.dart')) {
+        print('Current path ${item.path}');
+
+        final uri = item.uri;
+        final dartFile = await File.fromUri(uri).readAsLines();
+        lineCount += dartFile.length;
       }
     }
   }
-  return districtName;
-}
-
-bool pnpoly(int nvert, List<double> vertx, List<double> verty, double testx,
-    double testy) {
-  var c = false;
-  for (var i = 0, j = nvert - 1; i < nvert; j = i++) {
-    if (((verty[i] > testy) != (verty[j] > testy)) &&
-        (testx <
-            (vertx[j] - vertx[i]) * (testy - verty[i]) / (verty[j] - verty[i]) +
-                vertx[i])) {
-      c = !c;
-      print(i);
-    }
-  }
-  return c;
 }
