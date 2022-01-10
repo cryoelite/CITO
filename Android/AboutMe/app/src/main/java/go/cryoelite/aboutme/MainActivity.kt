937e94b66f23e20c7abb0d3e86f4979643a32353
Arvind Sagar <arvindsagar702@gmail.com>
2022-01-10 19:25:35 +0530

    fragment

--- Android/AboutMe/app/src/main/java/go/cryoelite/aboutme/MainActivity.kt
+++ Android/AboutMe/app/src/main/java/go/cryoelite/aboutme/MainActivity.kt
@@ -6,18 +6,29 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import go.cryoelite.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     lateinit var editText: EditText;
     lateinit var nickTextView: TextView;
     lateinit var doneButton: Button;
+    private val myName: MyName = MyName("Arvind Sagar!")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        editText = findViewById<EditText>(R.id.nickname_edit)
-        nickTextView = findViewById<TextView>(R.id.nickname_text)
-        doneButton = findViewById<Button>(R.id.done_button)
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.myName=myName;
+        editText = binding.nicknameEdit;
+        nickTextView = binding.nicknameText;
+        doneButton = binding.doneButton;
+
         doneButton.setOnClickListener {
             clickHandler(it);
         };
@@ -32,24 +43,27 @@ class MainActivity : AppCompatActivity() {
         doneButton.visibility=View.VISIBLE;
         editText.text.clear();
         editText.text.append(nickTextView.text);
-        nickTextView.text="";
+        myName.nickname="";
         nickTextView.visibility=View.GONE;
 
         editText.requestFocus();
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(editText, 0)
+        binding.invalidateAll();
+
     }
 
     private fun clickHandler(clickedView: View) {
         val text= editText.text.toString();
         if(text!= "" && text!= " ")
         {
-            nickTextView.text=text;
+            myName.nickname=text;
             editText.visibility=View.GONE;
             doneButton.visibility=View.GONE;
             nickTextView.visibility=View.VISIBLE;
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(clickedView.windowToken, 0)
+            binding.invalidateAll();
         }
 
     }
---
+++ Android/AboutMe/app/src/main/java/go/cryoelite/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package go.cryoelite.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
