ec03c023d1271ba0e724cfa4dd3a7b440cfaa508
Arvind Sagar <arvindsagar702@gmail.com>
2022-07-09 01:21:03 +0530

    more rustiness

--- Rust/citorust_alpha/src/main.rs
+++ Rust/citorust_alpha/src/main.rs
@@ -1,3 +1,18 @@
+
 fn main() {
-    println!("Hello, world!");
+    let mut x= 1;
+    let z = 'xyz_loop: loop {
+        if x==20 {
+           'two: loop {
+            if x==25 {
+                break 'two;
+            }
+            x+=1;
+           }
+           break 'xyz_loop x;
+        }
+        x+=1;
+    };
+    println!("{}", z);
 }
+
