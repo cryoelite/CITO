bc793d0f3f4c743b4b8b5c0cac01b9707f250ad3
Arvind Sagar <arvindsagar702@gmail.com>
2022-07-28 00:06:02 +0530

    Update main.rs

--- Rust/citorust_alpha/src/main.rs
+++ Rust/citorust_alpha/src/main.rs
@@ -1,47 +1,5 @@
-mod testaaaas {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+fn yo<T>(x:T){}
 
-    #[test]
-    fn yo() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
-
-
-use std::{collections::HashMap, fmt::Display};
-
-fn main() {
-    let a = String::from("yaa");
-    let mut b = HashMap::new();
-    b.insert("yo", a);
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-
-    let insertedValue = scores.entry(String::from("Yellow"));
-    let y: i32;
-
-    
-}
-
-
-pub trait Something {
-    fn Moo(&self) -> String {
-        return "y0o".to_string();
-    }
-}
-
-struct X<T> {
-    yo: T,
-}
-
-impl<T: Display> ToString for X<T> {
-    fn to_string(&self) -> String {
-        return "".to_string();
-    }
-    // --snip--
+fn main(){
+    let x= yo::<i32>(2);
 }
