14436c3e0003e6906416ce3480b557675cab5cad
cryoelite <itscryonim@gmail.com>
2026-02-22 16:29:06 +0530

    Flush

---
+++ React/tic-tac-toe/app/page.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+function Box() {
+  return <div className="box"></div>;
+}
+export default function Home() {
+  let boxes = Array.
+  
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
